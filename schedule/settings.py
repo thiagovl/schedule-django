166f2b3113e19dd780e5652f6b9a2d18ded0dd61
Thiago <lemes_vilarinho@yahoo.com.br>
2021-11-11 20:47:36 -0300

    Alterando os caminhos css

--- schedule/settings.py
+++ schedule/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_svm8kf91vl@2tyri@%m3e*@^$_^&wap2g_mqdf3)&sxpnemx'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-5_svm8kf91vl@2tyri@%m3e*@^$_^&wap2g_mqdf3)&sxpnemx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
