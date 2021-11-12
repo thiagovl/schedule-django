9dd473b0c45fae5fb0a2f7feea8d376af72c080d
Thiago <lemes_vilarinho@yahoo.com.br>
2021-11-12 10:41:07 -0300

    Criado a tela de Registro

--- core/views/view_crud.py
+++ core/views/view_crud.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from core.models import Evento 
+from django.http.response import Http404
+from django.contrib.auth.models import User
 
 # Visualizar evento
 @login_required(login_url='/login/')
@@ -48,3 +50,32 @@ def editar(request):
     if id_evento:
         dados['evento'] = Evento.objects.get(id=id_evento)
     return render(request, 'evento.html', dados)
+
+@login_required(login_url='/login/')
+def deletar(request, id):
+    try:
+        evento = Evento.objects.get(id=id)
+    except Exception:
+        raise Http404()
+
+    usuario = request.user
+    evento = Evento.objects.get(id=id)
+
+    if usuario == evento.usuario:
+        evento.delete()
+    else:
+        raise Http404()
+    return redirect('/')
+
+def cadastrar(request):
+    return render(request, 'signup.html')
+
+def registrar(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user=User.objects.create_user(nome, email=email, password=password)
+    user.is_superuser=False
+    user.is_staff=False
+    user.save()
+    return redirect('/')
